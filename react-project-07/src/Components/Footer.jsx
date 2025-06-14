dcba98b6ea5924d8f66576efad8642b9ffe549f5
mrjawadhuk7 <mrjawadhu@gmail.com>
2025-06-14 09:29:07 +0530

    version-1

---
+++ react-project-07/src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Footer extends  React.Component{
+  
+  render(){
+    
+    return(
+      <footer className="Footer">
+        <h2>Footer</h2>
+       
+      </footer>
+      
+    )
+  }
+}
+export default Footer;
